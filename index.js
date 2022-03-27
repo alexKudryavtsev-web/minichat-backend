3f4d3f260d345659980b7c9c51d7776bd2346758
alexKudryavtsev-web <qwertyshurazh@gmail.com>
2022-03-27 21:29:18 +0300

    1v: fixed bug

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ const cors = require("cors");
 const http = require("http");
 const mongoose = require("mongoose");
 require("dotenv").config();
-
 const io = require("./socket/socket.js");
-const HTTP_PORT = process.env.HTTP_PORT;
 
 const app = express();
 const httpServer = http.createServer(app);
@@ -14,22 +12,19 @@ io.attach(httpServer);
 app.use(express.json());
 app.use(
   cors({
-    origin: "*",
+    origin: process.env.CLIENT_URL,
     methods: ["GET", "POST"],
   })
 );
-app.get("/", (req, res) => {
-  res.json("Hello World");
-});
 
 async function start() {
   try {
+    const HTTP_PORT = process.env.HTTP_PORT;
     await mongoose.connect(
       `mongodb+srv://user:${process.env.DB_PASSWORD}@cluster0.lyps2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
     );
-
     httpServer.listen(HTTP_PORT, 511, () => {
-      console.log(`Server start on ${HTTP_PORT}`);
+      console.log(`Server started on ${HTTP_PORT}`);
     });
   } catch (e) {
     console.log(e);
--- socket/socket.js
+++ socket/socket.js
@@ -1,32 +1,48 @@
+const uuid = require("uuid");
 const { Server } = require("socket.io");
-const PORT = process.env.SOCKET_PORT;
+
+const history = [];
 
 const io = new Server({
-  port: PORT,
-  cors: { origin: "*" },
+  port: process.env.SOCKET_PORT,
+  cors: { origin: process.env.CLIENT_URL },
 });
 
 io.on("connection", (socket) => {
-  let currentName;
+  let currentUsername;
   socket.on("login", (name) => {
-    currentName = name;
-    io.emit("event", {
-      contentMessage: `Пользователь ${name} присоединился к беседе`,
-    });
+    currentUsername = name;
+    socket.emit("history", history);
+    if (currentUsername) {
+      sendAndSaveMessage({
+        message: `User ${name} has join`,
+        isEvent: true,
+      });
+    }
   });
 
   socket.on("message", (message) => {
-    io.emit("message", {
-      contentMessage: message,
-      name: currentName,
-    });
+    sendAndSaveMessage({ message, username: currentUsername, isEvent: false });
   });
 
   socket.on("disconnect", () => {
-    io.emit("event", {
-      contentMessage: `Пользователь ${currentName} вышел из беседы`,
-    });
+    if (currentUsername) {
+      sendAndSaveMessage({
+        message: `User ${currentUsername} has left`,
+        isEvent: true,
+      });
+    }
   });
 });
+
+function sendAndSaveMessage(messageObj) {
+  const data = {
+    ...messageObj,
+    date: Date.now(),
+    id: uuid.v4(),
+  };
+  history.push(data);
+  io.emit("message", data);
+}
 
 module.exports = io;
